2c1982f736541859c317d7df305ae4d8b7ccbafb
Kaan <kaan94karaca@gmail.com>
2018-11-30 00:28:22 +0300

    image modification. rotate, resize, color, conver

---
+++ pil_modification.py
@@ -0,0 +1,7 @@
+from PIL import Image, ImageFilter
+import os
+
+image1= Image.open('sevde1.jpg')
+# image1.rotate(90).save('sevde1_mod.jpg')
+# image1.convert(mode='L').save('sevde1_mod.jpg')
+# image1.filter(ImageFilter.GaussianBlur(15)).save('sevde1_mod.jpg')
